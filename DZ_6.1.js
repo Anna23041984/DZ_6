e510d00ec7560b463f31bfb70735df984fde602e
Anna23041984 <savchenkoanna2304@gmail.com>
2024-07-01 16:04:00 +0200

    Update DZ_6.1.js

--- DZ_6.1.js
+++ DZ_6.1.js
@@ -1,3 +1,15 @@
 //Створити функцію, яка прибирає з рядка всі символи, 
 //які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". 
 //Вихідний рядок та символи для видалення задає користувач.
+
+
+  function remove(str, start, end) {
+    return str.replace(/[^\s]/g, (_) => {
+      let n = _.charCodeAt()
+      return (n < start || n > end) ? '' : _
+    })
+    
+  }
+  
+  const str = "Виз3333hhначен754ня середmjkjkнього ариssgy7665фметич0olного"
+  console.log(remove(str, 1040, 1103))
